4e6e3c73b8d1c8206c005832ea1d3d7f5b64b194
Florin <florin.lilea94@gmail.com>
2017-05-27 11:02:07 +0200

    Added ClearID function

---
+++ quizzes/js/init.js
@@ -0,0 +1,21 @@
+function ClearId() {
+    var children = [];
+    delete root.id;
+
+    if (typeof root.children != "undefined") {
+        for (var i = 0; i < root.children.length; i++) {
+            children.push(root.children[i]);
+        }
+
+        while (children.length > 0) {
+            var temp = children.shift();
+            delete temp.id;
+
+            if (temp.children) {
+                for (var i = 0; i < temp.children.length; i++) {
+                    children.push(temp.children[i]);
+                }
+            }
+        }
+    }
+}
